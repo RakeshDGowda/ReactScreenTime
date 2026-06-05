09eb1f0db7fbaf98de09e656d10b1812e4fe99ef
RakeshDGowda <rakeshgowda9735@gmail.com>
2026-06-05 10:44:19 +0530

    vs studio code changes

--- src/components/Home/HomeTest.tsx
+++ src/components/Home/HomeTest.tsx
@@ -42,7 +42,7 @@ function HomeTest() {
 
   useEffect(() => {
     retrigger();
-    console.log("retrigrred");
+    console.log("retriggered");
   }, [user, id]);
 
   const fetchUsers = async (id: string) => {
